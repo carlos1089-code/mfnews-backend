c86dc75754b8c1a39c1013d09c82b76b7ef3aead
Carlos Nayi <carlos.nayi@mindfactory.ar>
2025-11-24 16:46:43 -0300

    fix: Minor bugs related to testing

--- src/news/news.controller.spec.ts
+++ src/news/news.controller.spec.ts
@@ -5,10 +5,23 @@ import { NewsService } from './news.service';
 describe('NewsController', () => {
   let controller: NewsController;
 
+  const mockNewsService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NewsController],
-      providers: [NewsService],
+      providers: [
+        { 
+          provide: NewsService,
+          useValue: mockNewsService  // Solo el mock, sin el servicio real
+        },
+      ],
     }).compile();
 
     controller = module.get<NewsController>(NewsController);
--- src/news/news.service.spec.ts
+++ src/news/news.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('NewsService', () => {
   let service: NewsService;
 
+  const mockPrismaService = {
+    news: {
+      findMany: jest.fn(),
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsService],
+      providers: [
+        NewsService,  // El servicio real está OK aquí
+        { 
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }, 
+      ],
     }).compile();
 
     service = module.get<NewsService>(NewsService);
